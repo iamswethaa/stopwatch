202df45f4f6f87681533e373b5ae1eaeeed9752c
iamswethaa <swethaelangovan733@gmail.com>
2024-11-11 17:00:55 +0530

    bg colour updated

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-3xl font-bold mb-4">Stopwatch</h1>
       <div className="text-5xl font-mono">{formatTime(time)}</div>
       <div className="flex space-x-4 mt-8">
